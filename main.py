037d2ae85e79b7c32f32cbd18386f091eb6dff1d
notshe4p <83170424+sleepntsheep@users.noreply.github.com>
2021-10-26 23:15:54 +0700

    Nova launcher, pyi spec

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 import os
@@ -37,8 +38,26 @@ class App(tk.Frame):
         b3 = Btn(
             self.parent,
             text='Install aurora store (google play alternative)',
-            command=self.auroraStore
+            command=lambda: self.installURL('https://files.auroraoss.com/AuroraStore/Stable/AuroraStore_4.0.7.apk')
         )
+
+        b4 = Btn(
+            self.parent,
+            text='Install nova launcher',
+            command=lambda: self.installURL('https://d-06.winudf.com/b/APK/Y29tLnRlc2xhY29pbHN3LmxhdW5jaGVyXzcwMDQ5XzFmNmIzMmU0?_fn=Tm92YSBMYXVuY2hlcl92Ny4wLjQ5X2Fwa3B1cmUuY29tLmFwaw&_p=Y29tLnRlc2xhY29pbHN3LmxhdW5jaGVy&am=hlzF95rRCIyp1GC2U_qQJQ&at=1635264415&k=22dbdd2e93eef4c7ec34d2222783875361797920')
+        )
+
+        self.status = tk.StringVar()
+        self.status.set('Choose an action')
+
+        tk.Label(
+            self.parent,
+            textvariable=self.status,
+            bg='red',
+            fg='white',
+            width=60,
+            height=2,
+            ).pack(side='bottom')
     
     def installAPK(self):
         path = fd.askopenfilename(
@@ -48,24 +67,26 @@ class App(tk.Frame):
         )
         path = path.replace(' ', '\\ ')
         print(path)
-        os.system('adb disconnect')
-        os.system('adb connect 127.0.0.1:58526')
+        subprocess.call(('adb disconnect'))
+        subprocess.call(('adb connect 127.0.0.1:58526'))
         self.apk(path)
+        self.status.set('Finished Installing APK')
 
     def adbShell(self):
-        os.system('adb disconnect')
-        os.system('adb connect 127.0.0.1:58526')
+        subprocess.call(('adb disconnect'))
+        subprocess.call(('adb connect 127.0.0.1:58526'))
         os.system('start cmd /k adb shell')
+        self.status.set('Finished opening adb shell')
 
-    def auroraStore(self):
-        link = 'https://files.auroraoss.com/AuroraStore/Stable/AuroraStore_4.0.7.apk'
-        os.system('adb disconnect')
-        os.system('adb connect 127.0.0.1:58526')
+    def installURL(self, link, name='app'):
+        subprocess.call(('adb disconnect'))
+        subprocess.call(('adb connect 127.0.0.1:58526'))
         with urllib3.PoolManager() as http:
             r = http.request('GET', link)
             with open('aurora.apk', 'wb') as f:
                 f.write(r.data)
         self.apk('aurora.apk')
+        self.status.set('Finished installing '+name)
         
     def apk(self, path):
         p = subprocess.getoutput('adb install '+path)
@@ -77,8 +98,11 @@ class App(tk.Frame):
             mb.showwarning('OK', p)
 
 if __name__ == '__main__':
+    WIDTH = 400
+    HEIGHT = 500
+
     root = tk.Tk()
-    root.geometry('400x500')
+    root.geometry(f'{WIDTH}x{HEIGHT}')
     root.title('WSA toolbox')
 
     HOME = os.path.expanduser('~')
